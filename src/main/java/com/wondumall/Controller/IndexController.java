84ef28839dccfaaf2f52e7059858a2d45d445494
jehohwang <jeho0809@naver.com>
2022-07-07 15:16:29 +0900

    My Account 클릭 후 login.do로 페이지 이동 기능 추가

--- src/main/java/com/wondumall/Controller/IndexController.java
+++ src/main/java/com/wondumall/Controller/IndexController.java
@@ -10,4 +10,24 @@ public class IndexController {
 	public String index() {
 		return "index";
 	}
+	
+	@GetMapping(value = "/blank.do")
+	public String blank() {
+		return "blank";
+	}
+	
+	@GetMapping(value = "/checkout.do")
+	public String checkout() {
+		return "checkout";
+	}
+	
+	@GetMapping(value = "/product.do")
+	public String product() {
+		return "product";
+	}
+	
+	@GetMapping(value = "/store.do")
+	public String store() {
+		return "store";
+	}
 }
