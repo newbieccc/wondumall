59b26356932747bc5e248aed943f737053e25ac5
SeungJun <ttws0107@gmail.com>
2022-07-08 16:40:31 +0900

    product login confirm test

--- src/main/java/com/wondumall/Controller/ProductController.java
+++ src/main/java/com/wondumall/Controller/ProductController.java
@@ -3,14 +3,17 @@ package com..Controller;
 import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import com..DTO.CategoryDTO;
+import com..DTO.LoginDTO;
 import com..DTO.ProductDTO;
 import com..Service.ProductService;
 import com..Util.Util;
@@ -24,6 +27,7 @@ public class ProductController {
 	@Autowired
 	private Util util;
 	
+	//제품 종류별 카테고리 분류하기
 	@RequestMapping(value = "/category")
 	public ModelAndView category(HttpServletRequest request) {
 		ModelAndView mv = new ModelAndView("category");
@@ -34,6 +38,7 @@ public class ProductController {
 			cate_no = util.str2Int(request.getParameter("cate_no"));
 		}
 		
+		//CategoryDTO에서 cate_no 값 가져오기
 		CategoryDTO dto = new CategoryDTO();
 		dto.setCate_no(cate_no);
 		
@@ -42,6 +47,23 @@ public class ProductController {
 		
 		mv.addObject("productList", productList);
 		mv.addObject("cate_no",cate_no);
+		return mv;
+	}
+	
+	
+	
+	@PostMapping(value = "/nav")
+	public ModelAndView productAdd(HttpServletRequest request) {
+		HttpSession session = request.getSession();
+		
+		LoginDTO dto = new LoginDTO();
+		
+		if(session.getAttribute("u_email") != null) {
+			
+		}
+		
+		ModelAndView mv = new ModelAndView("nav");
+		
 		return mv;
 	}
 }
